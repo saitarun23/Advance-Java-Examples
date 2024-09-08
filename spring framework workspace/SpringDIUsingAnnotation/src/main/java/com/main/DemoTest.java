a025d25d9053269f4db087716d361f5cc1e5308d
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-08 21:04:31 +0530

    done

--- spring framework workspace/SpringDIUsingAnnotation/src/main/java/com/main/DemoTest.java
+++ spring framework workspace/SpringDIUsingAnnotation/src/main/java/com/main/DemoTest.java
@@ -1,16 +1,27 @@
 package com.main;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.bean.Address;
 import com.bean.Employee;
+import com.config.MyConfiguration;
 
 public class DemoTest {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+//		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+//		Address add=(Address)ac.getBean("address");
+//		System.out.println(add);
+//		add.setCity("Bangalore");
+//		add.setState("Kar");
+//		System.out.println(add);
+//		Employee emp1 = (Employee)ac.getBean("employee");
+//		System.out.println(emp1);
+		
+		AnnotationConfigApplicationContext ac=new AnnotationConfigApplicationContext(MyConfiguration.class);
 		Address add=(Address)ac.getBean("address");
 		System.out.println(add);
 		add.setCity("Bangalore");
