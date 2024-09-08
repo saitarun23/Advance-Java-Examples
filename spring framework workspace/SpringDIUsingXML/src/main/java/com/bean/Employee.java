ce8202a11899ee9b9a0ea0920c6a093e5a607502
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-08 20:15:58 +0530

    done

--- spring framework workspace/SpringDIUsingXML/src/main/java/com/bean/Employee.java
+++ spring framework workspace/SpringDIUsingXML/src/main/java/com/bean/Employee.java
@@ -5,17 +5,33 @@ public class Employee {
 	private int id;
 	private String name;
 	private float salary;
-
-	public Employee(int id, String name, float salary) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-	}
-
-	public int getId() {
+	private Address add;
+		public int getId() {
 		return id;
 	}
+
+	public Employee(int id, String name, float salary, Address add) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.salary = salary;
+			this.add = add;
+		}
+
+	public Address getAdd() {
+			return add;
+		}
+
+		public void setAdd(Address add) {
+			this.add = add;
+		}
+
+	public Employee(int id, String name, float salary) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.salary = salary;
+		}
 
 	public void setId(int id) {
 		this.id = id;
@@ -37,16 +53,18 @@ public class Employee {
 		this.salary = salary;
 	}
 
-	public void display() {
-		System.out.println("Employee class method");
-	}
+		public void display() {
+			System.out.println("Employee class method");
+		}
 
-	public Employee() {
-		System.out.println("Employee Object method");
-	}
+		public Employee() {
+			super();
+			System.out.println("object created");
+		}
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+		@Override
+		public String toString() {
+			return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", add=" + add + "]";
+		}	
+		
 	}
-}
