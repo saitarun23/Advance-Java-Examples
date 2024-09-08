c0261f0dbe7a799f4dae72e2b67a97a8fc9ad3e0
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-08 23:28:43 +0530

    done

--- spring framework workspace/SpringWithDataSource/src/main/java/com/dao/EmployeeDao.java
+++ spring framework workspace/SpringWithDataSource/src/main/java/com/dao/EmployeeDao.java
@@ -1,3 +1,4 @@
+
 package com.dao;
 
 import java.sql.Connection;
@@ -9,51 +10,82 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.bean.Employee;
 
-@Repository
+@Repository						// singleton 
 public class EmployeeDao {
 	
-	@Autowired    	  //autowire search database connection using bytype in xml file.
-	DataSource ds;    //before spring framework we were doing configuration for data source in
-					  //application server like web logic,jboss etc. and using jndi lookupwe were searching
-					  //database connection
+	@Autowired	// autowire search database connection using byType in xml file. 
+	DataSource ds;// before spring framework we were doing configuration for data source in 
+			// application server like web logic, jboss etc. and using jndi lookup we were searching 
+				// database connection. 
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 	
 	public int storeEmployee(Employee emp) {
 		try {
-			Connection con=ds.getConnection();
-			PreparedStatement pstmt=con.prepareStatement("insert into employee values(?,?,?)");
-			pstmt.setInt(1, emp.getId());
-			pstmt.setString(2, emp.getName());
-			pstmt.setFloat(3, emp.getSalary());
-			return pstmt.executeUpdate();
+	return 
+	jdbcTemplate.update("insert into employee(id,name,salary) values(?,?,?)", emp.getId(),emp.getName(),emp.getSalary());
 		} catch (Exception e) {
 			System.err.println(e);
 			return 0;
 		}
 	}
+//	public int storeEmployee(Employee emp) {
+//		try {
+//			Connection con = ds.getConnection();
+//			PreparedStatement pstmt = con.prepareStatement("insert into employee(id,name,salary) values(?,?,?)");
+//			pstmt.setInt(1, emp.getId());
+//			pstmt.setString(2, emp.getName());
+//			pstmt.setFloat(3, emp.getSalary());
+//			return pstmt.executeUpdate();
+//		} catch (Exception e) {
+//			System.err.println(e);
+//			return 0;
+//		}
+//	}
+	
+//	public int updateEmployee(Employee emp) {
+//		try {
+//			Connection con = ds.getConnection();
+//			PreparedStatement pstmt = con.prepareStatement("update employee set salary = ? where id = ?");
+//			pstmt.setFloat(1, emp.getSalary());
+//			pstmt.setInt(2, emp.getId());
+//			return pstmt.executeUpdate();
+//		} catch (Exception e) {
+//			System.err.println(e);
+//			return 0;
+//		}
+//	}
 	
 	public int updateEmployee(Employee emp) {
 		try {
-			Connection con = ds.getConnection();
-			PreparedStatement pstmt = con.prepareStatement("update employee set salary = ? where id = ?");
-			pstmt.setFloat(1, emp.getSalary());
-			pstmt.setInt(2, emp.getId());
-			return pstmt.executeUpdate();
+return jdbcTemplate.update("update employee set salary = ? where id = ?", emp.getSalary(),emp.getId());
 		} catch (Exception e) {
 			System.err.println(e);
 			return 0;
 		}
 	}
 	
+//	public int deleteEmployee(int id) {
+//		try {
+//			Connection con = ds.getConnection();
+//			PreparedStatement pstmt = con.prepareStatement("delete from employee where id  = ?");
+//			pstmt.setInt(1, id);
+//			return pstmt.executeUpdate();
+//		} catch (Exception e) {
+//			System.err.println(e);
+//			return 0;
+//		}
+//	}
+	
 	public int deleteEmployee(int id) {
 		try {
-			Connection con = ds.getConnection();
-			PreparedStatement pstmt = con.prepareStatement("delete from employee where id  = ?");
-			pstmt.setInt(1, id);
-			return pstmt.executeUpdate();
+			return jdbcTemplate.update("delete from employee where id =?", id);
 		} catch (Exception e) {
 			System.err.println(e);
 			return 0;
