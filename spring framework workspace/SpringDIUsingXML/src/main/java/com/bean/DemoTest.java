668ce61fddb9b5044f80bd93e9d591cb7796cdde
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-08 19:28:25 +0530

    done

--- spring framework workspace/SpringDIUsingXML/src/main/java/com/bean/DemoTest.java
+++ spring framework workspace/SpringDIUsingXML/src/main/java/com/bean/DemoTest.java
@@ -12,7 +12,19 @@ public class DemoTest {
 		Resource res=new ClassPathResource("bean.xml");
 		BeanFactory obj=new XmlBeanFactory(res);
 		Employee e1=(Employee)obj.getBean("emp1");
+		Employee e2=(Employee)obj.getBean("emp2");
+		Employee e3=(Employee)obj.getBean("emp3");
 		e1.display();
+		e2.display();
+		e3.display();
+		
+		Connection c1=(Connection) obj.getBean("con1");
+		Connection c2=obj.getBean("con2", Connection.class);
+		Connection c3=obj.getBean("con1", Connection.class);
+		
+		System.out.println("c1 is "+c1+" and hashcode is:"+c1.hashCode());
+		System.out.println("c2 is "+c2+" and hashcode is:"+c2.hashCode());
+		System.out.println("c3 is "+c3+" and hashcode is:"+c3.hashCode());
 
 	}
 
