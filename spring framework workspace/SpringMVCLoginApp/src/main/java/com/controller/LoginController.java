d83d4919d33ab4b59eebcf0f63bf2a3e818bb2ad
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-14 23:25:16 +0530

    done

--- spring framework workspace/SpringMVCLoginApp/src/main/java/com/controller/LoginController.java
+++ spring framework workspace/SpringMVCLoginApp/src/main/java/com/controller/LoginController.java
@@ -2,13 +2,20 @@ package com.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bean.Login;
+import com.service.LoginService;
+
 @Controller
 public class LoginController {
+	
+	@Autowired
+	LoginService loginService;
 	
 	@RequestMapping(value = "checkLogin",method = RequestMethod.POST)
 	public ModelAndView checkLoginDetails(HttpServletRequest req) {  // DI for HttpServletReqeust
@@ -16,6 +23,25 @@ public class LoginController {
 		String password = req.getParameter("password");
 		ModelAndView mav = new ModelAndView();
 		if(emaiid.equals("akash@gmail.com") && password.equals("123")) {
+			mav.setViewName("success.jsp");
+		}else {
+			mav.setViewName("failure.jsp");
+		}
+		return mav;
+	}
+	
+	@RequestMapping(value = "signin",method = RequestMethod.POST)
+	public ModelAndView signFromDb(HttpServletRequest req,Login login) {  // DI for HttpServletReqeust and Login class. 
+		String emaiid = req.getParameter("emailid");
+		String password = req.getParameter("password");
+		
+		login.setEmailid(emaiid);
+		login.setPassword(password);
+		
+		String result = loginService.signIn(login);
+		
+		ModelAndView mav = new ModelAndView();
+		if(result.equals("success")) {
 			mav.setViewName("success.jsp");
 		}else {
 			mav.setViewName("failure.jsp");
--- spring framework workspace/SpringMVCLoginApp/src/main/webapp/login.jsp
+++ spring framework workspace/SpringMVCLoginApp/src/main/webapp/login.jsp
@@ -7,8 +7,18 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h2>Login Page</h2>
+<h2>Login Page with hardcoding</h2>
 <form action="checkLogin" method="post">
+<label>EmailId</label>
+<input type="email" name="emailid"/><br/>
+<label>Password</label>
+<input type="password" name="password"/><br/>
+<input type="submit" value="SignIn"/>
+<input type="reset" value="reset"/>
+</form>
+<hr/>
+<h2>Login Page From Db</h2>
+<form action="signin" method="post">
 <label>EmailId</label>
 <input type="email" name="emailid"/><br/>
 <label>Password</label>
