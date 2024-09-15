3e690759f1530c755d0349b38f5f8ddb3363680c
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-15 22:04:25 +0530

    done

--- spring boot workspace/SpringBootSimpleApp/src/main/java/com/controller/MyController.java
+++ spring boot workspace/SpringBootSimpleApp/src/main/java/com/controller/MyController.java
@@ -1,8 +1,16 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
 
+	// http://localhost:9090
+		@RequestMapping(value = "",method = RequestMethod.GET)
+		public String openPage() {
+			System.out.println("before index page open this method called.");
+			return "index";		// using view resolver it search page inside templates model.
+		}
 }
