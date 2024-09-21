3c98b944c0f6d3f342cc09c9b5ecbec129ac52e5
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-21 22:21:42 +0530

    done

---
+++ spring boot workspace/spring-boot-simple-web-app/src/main/java/com/SpringBootSimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleWebAppApplication.class, args);
+	}
+
+}
