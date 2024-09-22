0f6838aaa40df282f147575e05de1b4a65f68207
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-22 20:07:27 +0530

    done

--- spring boot workspace/sb-web-app-thymeleaf/src/main/java/com/controller/LoginController.java
+++ spring boot workspace/sb-web-app-thymeleaf/src/main/java/com/controller/LoginController.java
@@ -5,16 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bean.Login;
+
 @Controller
 public class LoginController {
 	
 	@RequestMapping(value = "",method = RequestMethod.GET)
-	public String openPage(Model mm) {
-		mm.addAttribute("msg", "Simplilearn");
+	public String openPage(Model mm, Login ll) {  // DI for Model API part of spring framework  as well as Login object. 
+		mm.addAttribute("msg", "SAI TARUN");  // request.setAttribute("msg","Akash");
 		mm.addAttribute("id", 100);
-		mm.addAttribute("name", "Sai Tarun");
-		mm.addAttribute("age", 24);
+		mm.addAttribute("name", "saitarun");
+		mm.addAttribute("age", 21);
+		mm.addAttribute("login", ll);		// ll object store in model scope. 
 		return "index";
 	}
 
+	@RequestMapping(value = "checkLogin",method = RequestMethod.POST)
+	public String checkLoginDetails(Model mm, Login ll) {
+		if(ll.getEmailid().equals("akash@gmail.com") && ll.getPassword().equals("123")) {
+			mm.addAttribute("result", "akash@gmail.com");
+			return "success";
+		}else {
+			return "failure";
+		}
+	}
 }
