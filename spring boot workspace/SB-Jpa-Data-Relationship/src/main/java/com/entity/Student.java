0fb221311c293e4a62c56c49fe68fdf79762ff3d
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-28 22:29:17 +0530

    done

---
+++ spring boot workspace/SB-Jpa-Data-Relationship/src/main/java/com/entity/Student.java
@@ -0,0 +1,42 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int sid;
+	private String sname;
+	private int age;
+	private Integer tsid;    	//it can hold null value
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Integer getTsid() {
+		return tsid;
+	}
+	public void setTsid(Integer tsid) {
+		this.tsid = tsid;
+	}
+	
+}
