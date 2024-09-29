bb548af808a11df35dc095a2d7853025b039a801
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-29 21:07:25 +0530

    done

--- spring boot workspace/Spring-boot-restapi/src/main/java/com/SpringBootRestapiApplication.java
+++ spring boot workspace/Spring-boot-restapi/src/main/java/com/SpringBootRestapiApplication.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com")
+@SpringBootApplication(scanBasePackages = "com")	// enable @RestController, @Service, @Repository
+@EntityScan(basePackages = "com.entity")			// enable @Entity annotation
 public class SpringBootRestapiApplication {
 
 	public static void main(String[] args) {
