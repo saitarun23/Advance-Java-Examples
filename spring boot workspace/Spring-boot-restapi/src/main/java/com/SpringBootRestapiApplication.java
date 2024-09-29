faa39ce0dd741ea800d77505acb5ca7e82b8999e
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-29 20:27:39 +0530

    done

--- spring boot workspace/Spring-boot-restapi/src/main/java/com/SpringBootRestapiApplication.java
+++ spring boot workspace/Spring-boot-restapi/src/main/java/com/SpringBootRestapiApplication.java
@@ -3,11 +3,12 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
 public class SpringBootRestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestapiApplication.class, args);
+		System.out.println("Spring boot rest api up!");
 	}
 
 }
---
+++ spring boot workspace/Spring-boot-restapi/src/main/java/com/controller/SimpleController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SimpleController {
+	
+	@RequestMapping(value="hello",method = RequestMethod.GET)
+	public @ResponseBody String Hello() {
+		return "welcome to spring boot rest api";
+	}
+
+}
