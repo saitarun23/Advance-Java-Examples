0dbb45aa72483e04d794fd94b69943b81c6d3238
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-09-29 22:03:06 +0530

    done

--- spring boot workspace/Spring-boot-restapi/src/main/java/com/controller/ProductController.java
+++ spring boot workspace/Spring-boot-restapi/src/main/java/com/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,25 @@ import com.service.ProductService;
 public class ProductController {
 
 	@Autowired
-	ProductService prodcutService;
+	ProductService productService;
 	
 	// http://localhost:8080/find_products
 	@RequestMapping(value = "find_products",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Product> findAllProducts(){
-		return prodcutService.findAllProducts();
+		return productService.findAllProducts();
 	}
+	
+	// http://localhost:8080/find_product_by_id/1		method is get with path param 
+	
+		@RequestMapping(value = "find_product_by_id/{id}",method = RequestMethod.GET)
+		public String findProductById(@PathVariable("id") int pid) {
+			return productService.findProductByIdAsString(pid);
+		}
+		
+		// http://localhost:8080/find_product_by_id_object/1		method is get with path param 
+		
+		@RequestMapping(value = "find_product_by_id_object/{id}",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+		public Product findProductByIdReturnProduct(@PathVariable("id") int pid) {
+				return productService.findProductByIdAsObject(pid);
+		}
 }
