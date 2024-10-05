5b79d6bb1dea48ba27b029a8a22b95495334a22d
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-05 20:36:47 +0530

    done

--- Spring Micro Service/second_micro-service/src/main/java/com/SecondMicroServiceApplication.java
+++ Spring Micro Service/second_micro-service/src/main/java/com/SecondMicroServiceApplication.java
@@ -2,12 +2,15 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EnableDiscoveryClient
 public class SecondMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondMicroServiceApplication.class, args);
+		System.err.println("2nd micro service running on port number 9292");
 	}
 
 }
