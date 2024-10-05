75cc81bd7960d5ff50760ae34ad49ebfa7f83b29
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-05 20:23:20 +0530

    done

---
+++ Spring Micro Service/first_micro-service/src/main/java/com/FirstMicroServiceApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = "com")
+@EnableDiscoveryClient
+public class FirstMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMicroServiceApplication.class, args);
+	}
+
+}
