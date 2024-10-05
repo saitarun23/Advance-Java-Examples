2d6b924512fe541a4b876bf9d645af1258e7d960
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-05 21:52:05 +0530

    done

--- Spring Micro Service/account_micro_service/src/main/java/com/AccountMicroServiceApplication.java
+++ Spring Micro Service/account_micro_service/src/main/java/com/AccountMicroServiceApplication.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EnableDiscoveryClient
+@EntityScan(basePackages = "com.entity")
+@EnableJpaRepositories(basePackages = "com.repository")
 public class AccountMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMicroServiceApplication.class, args);
+		System.err.println("account micro service up on port number 9393");
 	}
 
 }
--- Spring Micro Service/account_micro_service/src/main/java/com/service/AccountService.java
+++ Spring Micro Service/account_micro_service/src/main/java/com/service/AccountService.java
@@ -1,5 +1,71 @@
 package com.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.entity.Account;
+import com.repository.AccountRepository;
+
+@Service
 public class AccountService {
 
+	@Autowired
+	AccountRepository accountRepository;
+	
+	public String createAccount(Account account) {
+		if(accountRepository.existsById(account.getAccno())) {
+			return "Account number must be unique";
+		}else {
+			accountRepository.save(account);
+			return "Account created successfully";
+		}
+	}
+	
+	public String findBalance(int accno) {
+		Optional<Account> result = accountRepository.findById(accno);
+		if(result.isPresent()) {
+			Account acc = result.get();
+			return "Your balance is "+acc.getAmount();
+		}else {
+			return "Account not exists";
+		}
+	}
+	
+	public String withdraw(Account account) {		// accno and amount 
+		Optional<Account> result = accountRepository.findById(account.getAccno());
+		if(result.isPresent()) {
+			Account acc = result.get();
+			if(acc.getAmount()>=account.getAmount()) {
+				acc.setAmount(acc.getAmount()-account.getAmount());
+				accountRepository.saveAndFlush(acc);
+				return "Withdraw amount successfully";
+			}else {
+				return "Can't withdrawn no amount present";
+			}
+		}else {
+			return "Account not exists";
+		}
+	}
+	
+	public String deposit(Account account) {		// accno and amount 
+		Optional<Account> result = accountRepository.findById(account.getAccno());
+		if(result.isPresent()) {
+			Account acc = result.get();
+			acc.setAmount(acc.getAmount()+account.getAmount());
+			accountRepository.saveAndFlush(acc);
+			return "Withdraw amount successfully";
+		}else {
+			return "Account not exists";
+		}
+	}
+	
+	public String findBalanceUsingEmailId(String emailid) {
+		try {
+			return "Your balance is "+accountRepository.findBalanceByEmailId(emailid);
+		} catch (Exception e) {
+			return "Account not exists with emailid as "+emailid;
+		}
+	}
 }
